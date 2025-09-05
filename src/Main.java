e7f26152369eac998da435bda45270709979c0fb
zman43g <zman3@yandex.ru>
2025-09-05 10:06:39 +0700

    ДЗ №9

--- src/Main.java
+++ src/Main.java
@@ -84,6 +84,7 @@ public class Main {
         for (i = 0; i < 2125; i += 79) {
         if (i>=1825) System.out.println(i);}
 
+
         /*short currentYear = 1825;
         while (currentYear <= 2125) {
             if (currentYear % 79 == 0) {
